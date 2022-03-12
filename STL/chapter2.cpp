c733dc1a84d5bc45b2159dd0b059a576e22f6962
MI <1143016001@qq.com>
2022-03-13 00:05:21 +0800

    second

--- STL/chapter2.cpp
+++ STL/chapter2.cpp
@@ -9,7 +9,7 @@ int main(){
     int ia[6] = {27, 210, 12, 47, 109, 83};
     vector<int, allocator<int>> vi(ia, ia+6);
 
-    cout << count_if(vi.begin(), vi.end(), 
+    cout << count_if(vi.begin(), vi.end(),      //算法：计算符合条件的个数
             not1(bind2nd(less<int>(),40)));
             
     return 0;
